5270802aa058f305b34caafe8c66659db4096cd0
Thiago Bezerra <bezerrath@gmail.com>
2020-04-30 16:53:57 -0300

    Altera pasta dos arquivos de mutation

--- app/GraphQL/Mutations/LoginMutation.php
+++ app/GraphQL/Mutations/LoginMutation.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\GraphQL\Resolvers;
+namespace App\GraphQL\Mutations;
 
 use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
@@ -8,18 +8,18 @@ use Nuwave\Lighthouse\Exceptions\AuthenticationException;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 
-class LoginResolver
+class LoginMutation
 {
-
     /**
-     * @param $rootValue
-     * @param array $args
-     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext|null $context
-     * @param \GraphQL\Type\Definition\ResolveInfo $resolveInfo
-     * @return array
-     * @throws \Exception
+     * Return a value for the field.
+     *
+     * @param  null  $rootValue Usually contains the result returned from the parent field. In this case, it is always `null`.
+     * @param  mixed[]  $args The arguments that were passed into the field.
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Arbitrary data that is shared between all fields of a single query.
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Information about the query itself, such as the execution state, the field name, path to the field from the root, and more.
+     * @return mixed
      */
-    public function resolve($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
+    public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
         $credentials = [
             'email'=>$args['input']['email'],
@@ -37,11 +37,10 @@ class LoginResolver
         return [
             'usuario'=>auth()->user(),
             'status'=>200,
-            'message'=>'OK',
+            'message'=>'Autenticação realizada',
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60
         ];
     }
-
 }
